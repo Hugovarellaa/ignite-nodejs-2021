0f2c2e1f78e8f2eb5acb65198d7086ce07665406
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-29 19:30:44 -0300

    Cadastrando usuario pelo cpf e validando

--- chapter-01/src/server.ts
+++ chapter-01/src/server.ts
@@ -1,9 +1,32 @@
 import express from 'express'
+import { v4 as uuidV4 } from 'uuid'
 
 const app = express()
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const customers = []
+
+app.post('/account', (request, response) => {
+  const { name, cpf } = request.body
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  )
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: 'Customer already exists' })
+  }
+
+  const account = {
+    id: uuidV4(),
+    name,
+    cpf,
+    statement: [],
+  }
+
+  customers.push(account)
+
+  return response.status(201).json(account)
 })
 
 app.listen(3333, () => console.log('listening on port 3333'))
