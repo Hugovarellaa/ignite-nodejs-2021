1ca575458ef2d0143898471be2f588b410e71880
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-30 14:53:23 -0300

    Readme

--- 01-fini-api/src/server.ts
+++ 01-fini-api/src/server.ts
@@ -1,9 +1,31 @@
 import express from 'express'
+import { v4 as uuidV4 } from 'uuid'
 
 const app = express()
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+type Account = {
+    id: string
+    name: string
+    cpf: string
+    statement: string[]
+}
+
+const customer = []
+
+app.post('/account', (request, response) => {
+    const { name, cpf } = request.body
+
+    const account: Account = {
+        id: uuidV4(),
+        name,
+        cpf,
+        statement: []
+    }
+
+    customer.push(account)
+
+    return response.status(201).json(account)
 })
 
 app.listen(3333, () => console.log('Listening on port 3333'))
