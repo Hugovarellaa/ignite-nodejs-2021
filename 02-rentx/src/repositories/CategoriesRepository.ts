149c79d1e04bdd217a13152e55fb9692bb8a96e6
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-30 20:31:49 -0300

    Create categories

--- 02-rentx/src/repositories/CategoriesRepository.ts
+++ 02-rentx/src/repositories/CategoriesRepository.ts
@@ -1,9 +1,25 @@
 import { Category } from '../model/Category'
 
+interface ICreateCategoryDTO {
+  name: string
+  description: string
+}
+
 class CategoriesRepository {
   private categories: Category[]
   constructor() {
     this.categories = []
+  }
+  create({ name, description }: ICreateCategoryDTO) {
+    const category: Category = new Category()
+
+    Object.assign(category, {
+      name,
+      description,
+      createdAt: new Date(),
+    })
+
+    this.categories.push(category)
   }
 }
 
