99342d9a6b8c55c899a366daf0a013f36a02f26c
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-12-01 17:54:22 -0300

    Controller

--- 02-rentx/src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ 02-rentx/src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -1,3 +1,11 @@
-class ImportCategoryController {}
+import { Request, Response } from 'express'
+
+class ImportCategoryController {
+  handle(request: Request, response: Response): Response {
+    const { file } = request
+    console.log(file)
+    return response.send()
+  }
+}
 
 export { ImportCategoryController }
