dea6b68be1f054444f95108fd0ead0419697434d
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-12-01 14:58:53 -0300

    List Specification Controller

--- 02-rentx/src/modules/cars/useCases/listSpecifications/ListSpecificationController.ts
+++ 02-rentx/src/modules/cars/useCases/listSpecifications/ListSpecificationController.ts
@@ -1,3 +1,14 @@
-class ListSpecificationController {}
+import { Request, Response } from 'express'
+
+import { ListSpecificationUseCase } from './ListSpecificationUseCase'
+
+class ListSpecificationController {
+  constructor(private listSpecificationUseCase: ListSpecificationUseCase) {}
+  handle(request: Request, response: Response) {
+    const specifications = this.listSpecificationUseCase.execute()
+
+    return response.status(200).json(specifications)
+  }
+}
 
 export { ListSpecificationController }
