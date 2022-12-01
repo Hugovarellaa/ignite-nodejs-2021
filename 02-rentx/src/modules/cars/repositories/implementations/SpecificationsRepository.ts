442e080ca8ab278a387cb6a85a6918fde5f91ee7
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-12-01 13:19:36 -0300

    Services specification

--- 02-rentx/src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ 02-rentx/src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -10,6 +10,7 @@ class SpecificationsRepository implements ISpecificationsRepository {
   constructor() {
     this.specifications = []
   }
+
   create({ name, description }: ICreateSpecificationDTO): void {
     const specification = new Specification()
     Object.assign(specification, {
@@ -19,6 +20,12 @@ class SpecificationsRepository implements ISpecificationsRepository {
     })
 
     this.specifications.push(specification)
+  }
+  findByName(name: string): Specification {
+    const specification = this.specifications.find(
+      (specification) => specification.name === name
+    )
+    return specification
   }
 }
 
--- 02-rentx/src/modules/cars/services/CreateSpecificationService.ts
+++ 02-rentx/src/modules/cars/services/CreateSpecificationService.ts
@@ -8,6 +8,13 @@ interface IRequest {
 class CreateSpecificationService {
   constructor(private specificationRepository: SpecificationsRepository) {}
   execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists =
+      this.specificationRepository.findByName(name)
+
+    if (specificationAlreadyExists) {
+      throw new Error(`Specification ${name} already exists!`)
+    }
+
     this.specificationRepository.create({ name, description })
   }
 }
