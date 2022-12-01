21370f666dd2a4987f71a727e657ec6da42995c4
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-12-01 18:32:18 -0300

    Import Category for csv ok

--- 02-rentx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ 02-rentx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -11,7 +11,7 @@ interface IImportCategory {
 class ImportCategoryUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) {}
 
-  loadCategory(file: Express.Multer.File) {
+  loadCategory(file: Express.Multer.File): Promise<IImportCategory[]> {
     return new Promise((resolve, reject) => {
       const stream = fs.createReadStream(file.path)
       const csvParse = parse()
@@ -37,7 +37,18 @@ class ImportCategoryUseCase {
 
   async execute(file: Express.Multer.File) {
     const categories = await this.loadCategory(file)
-    console.log(categories)
+
+    categories.map(async (category) => {
+      const { name, description } = category
+
+      const categoryExists = this.categoriesRepository.findByName(name)
+      if (!categoryExists) {
+        this.categoriesRepository.create({
+          name,
+          description,
+        })
+      }
+    })
   }
 }
 
