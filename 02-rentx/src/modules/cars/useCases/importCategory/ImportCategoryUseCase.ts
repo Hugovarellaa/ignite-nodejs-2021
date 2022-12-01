64f00dfe9f14772936b8aaf9959ceef90960a3ea
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-12-01 18:10:59 -0300

    Lendo arquivo csv

--- 02-rentx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ 02-rentx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,16 @@
+import { parse } from 'csv-parse'
+import fs from 'fs'
+
 class ImportCategoryUseCase {
   execute(file: Express.Multer.File) {
-    console.log(file)
+    const stream = fs.createReadStream(file.path)
+    const csvParse = parse()
+
+    stream.pipe(csvParse)
+
+    csvParse.on('data', async (row: any) => {
+      console.log(row)
+    })
   }
 }
 
