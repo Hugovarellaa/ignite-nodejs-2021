a2d4d2907fa119aeab2ec5f1aba67f47669ad9de
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-12-01 18:28:09 -0300

    Bug promise

--- 02-rentx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ 02-rentx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -11,23 +11,32 @@ interface IImportCategory {
 class ImportCategoryUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) {}
 
-  loadcategory(file: Express.Multer.File) {
-    const stream = fs.createReadStream(file.path)
-    const csvParse = parse()
+  loadCategory(file: Express.Multer.File) {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path)
+      const csvParse = parse()
 
-    const categories: IImportCategory[] = []
+      const categories: IImportCategory[] = []
 
-    stream.pipe(csvParse)
+      stream.pipe(csvParse)
 
-    csvParse.on('data', async (row) => {
-      const [name, description] = row
-      categories.push({ name, description })
+      csvParse
+        .on('data', async (row) => {
+          const [name, description] = row
+          categories.push({ name, description })
+        })
+        .on('end', () => {
+          fs.promises.unlink(file.path)
+          resolve(categories)
+        })
+        .on('error', (err) => {
+          reject(err)
+        })
     })
-    return categories
   }
 
-  execute(file: Express.Multer.File) {
-    const categories = this.loadcategory(file)
+  async execute(file: Express.Multer.File) {
+    const categories = await this.loadCategory(file)
     console.log(categories)
   }
 }
