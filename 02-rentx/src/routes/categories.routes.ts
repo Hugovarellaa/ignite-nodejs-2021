6b7bdf96b0971013a0710d761d31f86f673a7e3b
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-12-01 14:24:12 -0300

    ListCategories in Get

--- 02-rentx/src/routes/categories.routes.ts
+++ 02-rentx/src/routes/categories.routes.ts
@@ -1,18 +1,15 @@
 import { Router } from 'express'
 
-import { CategoriesRepository } from '../modules/cars/repositories/implementations/CategoriesRepository'
 import { createCategoryController } from '../modules/cars/useCases/createCategory'
+import { listCategoriesController } from '../modules/cars/useCases/listCategories'
 
 const categoriesRoutes = Router()
-const categoriesRepository = new CategoriesRepository()
 
 categoriesRoutes.post('/', (request, response) => {
   createCategoryController.handle(request, response)
 })
 
 categoriesRoutes.get('/', (request, response) => {
-  const all = categoriesRepository.list()
-
-  return response.json(all)
+  listCategoriesController.handle(request, response)
 })
 export { categoriesRoutes }
