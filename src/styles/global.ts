d5b00d2247b01f5b31ecddfaeafc1ed3e8f9ae6f
hugo <hugovarellaa@gmail.com>
2023-01-06 12:19:44 -0300

    Estilos globais

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -13,5 +13,23 @@ export const globalStyles = globalCss({
     color: "$gray100",
   },
 
-  "body, input, input, select, textarea": {},
+  "body, input, input, select, textarea": {
+    fontFamily: "Roboto sans-serif",
+    fontSize: "1rem",
+    fontWeight: 400,
+  },
+
+  button: {
+    cursor: "pointer",
+  },
+
+  a: {
+    textDecoration: "none",
+    color: "inherit",
+  },
+
+  "[disabled]": {
+    opacity: 0.7,
+    cursor: "not-allowed",
+  },
 });
