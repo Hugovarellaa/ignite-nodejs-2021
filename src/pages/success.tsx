3c227288a25ded79781c51261f1588870893c135
hugo <hugovarellaa@gmail.com>
2023-01-06 18:47:55 -0300

    Layout success pages

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,7 +1,23 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
 export default function Success() {
   return (
-    <div>
-      <h1>Success</h1>
-    </div>
+    <SuccessContainer>
+      <h1>Compra efetuada</h1>
+      <ImageContainer>
+        <img src="" alt="" />
+      </ImageContainer>
+
+      <p>
+        Uhuul <strong>Diego Fernandes</strong>, sua{" "}
+        <strong>Camiseta Beyond the Limits</strong>
+        já está a caminho da sua casa.{" "}
+      </p>
+
+      <Link href="/">
+        <span>Voltar ao catálogo</span>
+      </Link>
+    </SuccessContainer>
   );
 }
