3b739cdaf4beb5614363c9c72949ea6f92100184
hugo <hugovarellaa@gmail.com>
2023-01-06 13:18:07 -0300

    Layout home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,32 @@
-import { styled } from "../styles";
+import Image from "next/image";
+import { HomeContainer, HomeProduct } from "../styles/pages/home";
 
-const Button = styled("button", {
-  display: "inline-block",
-  border: "none",
-  padding: "0.5rem 1rem",
-  borderRadius: "0.25rem",
-  fontSize: "0.875rem",
-  fontWeight: "bold",
-  backgroundColor: "$gree300",
-  color: "$white",
-});
+// import camiseta01 from "../assets/img01.png";
+import camiseta02 from "../assets/img02.png";
+import camiseta03 from "../assets/img03.png";
+// import camiseta04 from "../assets/img04.png";
+// import camiseta05 from "../assets/img05.png";
 
 export default function Home() {
   return (
     <>
-      <main>
-        <Button>Adicionar</Button>
-      </main>
+      <HomeContainer>
+        <HomeProduct>
+          <Image src={camiseta02} alt="camiseta01" width={520} height={480} />
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </HomeProduct>
+
+        <HomeProduct>
+          <Image src={camiseta03} alt="camiseta01" width={520} height={480} />
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </HomeProduct>
+      </HomeContainer>
     </>
   );
 }
