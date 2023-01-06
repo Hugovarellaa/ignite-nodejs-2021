1fc645e537c5175606c3eab04cc4e57f101206e8
hugo <hugovarellaa@gmail.com>
2023-01-06 12:01:08 -0300

    testando stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,21 @@
+import { styled } from "../styles";
+
+const Button = styled("button", {
+  display: "inline-block",
+  border: "none",
+  padding: "0.5rem 1rem",
+  borderRadius: "0.25rem",
+  fontSize: "0.875rem",
+  fontWeight: "bold",
+  backgroundColor: "$gree300",
+  color: "$white",
+});
+
 export default function Home() {
   return (
     <>
       <main>
-        <h1>ola mundo</h1>
+        <Button>Adicionar</Button>
       </main>
     </>
   );
